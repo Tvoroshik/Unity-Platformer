495b30791a0db7ece54eea3b1b08a83c15d7f8e7
Tvoroshik <tvoroshik@yandex.ru>
2021-09-13 11:42:16 +0500

    New player anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public BoxCollider2D boxCollider2D;
     public BoxCollider2D boxCollider2;
     SpriteRenderer sr;
+    int punchCount;
 
 
     private void Start()
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
+        if (Input.GetKey(KeyCode.S))
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
         if (Input.GetAxis("Horizontal") == 0)
             anim.SetInteger("anim", 0);
         if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
@@ -30,14 +32,49 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;
         else if (Input.GetAxis("Horizontal") < 0)
             sr.flipX = true;
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
             Crouch();
         if (Input.GetKeyUp(KeyCode.S))
             CrouchOff();
+        if (Input.GetKey(KeyCode.S) && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)))
+            CrouchKick();
         if (Input.GetKeyDown(KeyCode.W) && ground == true)
             Jump();
+        if (ground == false && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)))
+            FlyingKick();
+        else if (ground == false && (!Input.GetKeyDown(KeyCode.A) || !Input.GetKeyDown(KeyCode.D)))
+            anim.SetInteger("anim", 5);
+        if (Input.GetKeyDown(KeyCode.W) && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)))
+            Kick();
+       if (Input.GetMouseButton(0))
+        {
+            Punch();
+        }
+
 
     }
+  
+    void Punch()
+    {
+        anim.SetInteger("anim", 9);
+     }
+    void Kick()
+    {
+        anim.SetInteger("anim", 8);
+    }
+    void Hurt()
+    {
+        anim.SetInteger("anim", 7);
+    }
+    void FlyingKick()
+    {
+        anim.SetInteger("anim", 6);
+    }
+    void CrouchKick()
+    {
+        anim.SetInteger("anim", 4);
+            }
+   
     void Crouch()
     {
         anim.SetInteger("anim", 3);
