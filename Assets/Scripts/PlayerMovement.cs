e39226e00e8600065dc8951546afd76bb252b756
Tvoroshik <tvoroshik@yandex.ru>
2022-07-31 16:31:14 +0500

    add assets for player and config animations for him

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    
-   private Rigidbody2D body;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+
 
     private void Awake()
     {
+        //Grab references for rigitbody and animator
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizonalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizonalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Flip left or right
+        if (horizonalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizonalInput < -0.01f)
+            transform.localScale = new Vector3(-1,1,1);
+
+         
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //Set animator param
+        anim.SetBool("run", horizonalInput != 0);
+        anim.SetBool("grounded", grounded);
+
     }
 
-   
+    private void Jump()
+    {  
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+
+    }
 }
 
